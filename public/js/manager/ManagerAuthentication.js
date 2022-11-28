6e862bbf186bb8e3c53b79eebb9482f80b005098
jerry-li65 <jerry.li65@tamu.edu>
2022-11-28 11:21:37 -0600

    try again prompt

--- public/js/manager/ManagerAuthentication.js
+++ public/js/manager/ManagerAuthentication.js
@@ -1,9 +1,23 @@
-function manEmailSubmitted() {
+function manPassSubmitted() {
   manPassword = document.getElementById("man-pw").value;
   if (manPassword == "CorrectHorseBatteryStapl3"){
     window.location.href = "./ManagerOptions.html";
   }
   else{
-    window.location.href = "../other/UserSelector.html";
+    // window.location.href = "../other/UserSelector.html";
+
+    //  var table = "<table>";
+    //  table += "<tr>";
+    //  table += "<th><center>Title</center></th>";
+    //  table += "</tr>";
+    //  table += "<tr>";
+    //  table += "<td><center> data </center></td>";
+    //  table += "</tr>";
+    //  table += "</table>";
+    //  $("body").append(table);
+
+    const youre_wrong = document.getElementById("incorrect-pw");
+    youre_wrong.innerHTML = "Try again.";
   }
+
 }
