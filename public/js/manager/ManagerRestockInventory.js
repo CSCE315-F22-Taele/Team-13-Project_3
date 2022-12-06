1f7a3cda4202ee0b4f4cab63f4ac913c50e90d2f
jerry-li65 <jerry.li65@tamu.edu>
2022-12-06 16:47:35 -0600

    manager restock additional functionality template

--- public/js/manager/ManagerRestockInventory.js
+++ public/js/manager/ManagerRestockInventory.js
@@ -1,5 +1,11 @@
 var stringOfItemsNeedingRestock = "";
 
+var itemName = "";
+var itemQuantityToRestock = "";
+
+/**
+ * Functionality 1: Shows the items that need to be restocked
+ */
 function checkInventoryRestock(){
     stringOfItemsNeedingRestock += "";
     /* DATABASE CODE HERE
@@ -9,4 +15,17 @@ function checkInventoryRestock(){
 
     const displayItems = document.getElementById("restockItemsDisplay");
     displayItems.innerHTML = stringOfItemsNeedingRestock;
+}
+
+/**
+ * Functionality 2: Allows manager to restock a specific item
+ */
+function quantityItemSubmitted(){
+    itemName = document.getElementById("itemName").value;
+    itemQuantityToRestock = document.getElementById("quantityAdded").value;
+
+    // DATABASE CODE HERE
+
+    const displayQuantityAdded = document.getElementById("restockQuantityItemsDisplay");
+    displayQuantityAdded.innerHTML = "Updated Quantity!";
 }
