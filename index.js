a14e85ce345b896bd67694515b127a8b2b61cfb0
jerry-li65 <jerry.li65@tamu.edu>
2022-12-09 00:55:05 -0600

    possibly working db render

--- index.js
+++ index.js
@@ -51,3 +51,83 @@ app.get("/oauth/redirect", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { response } = require("express");
+dotenv.config();
+
+const dbConnect = require("./dbConnect");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// create
+app.post("/addItem", (req, res) => {
+  console.log("item type: " + req.body.itemType);
+  console.log("item name: " + req.body.itemName);
+  console.log("item price: " + req.body.itemPrice);
+
+  const db = dbConnect.getDbConnectInstance();
+  try {
+    db.addItem(req.body.itemType, req.body.itemName, req.body.itemPrice);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/updatePrice", (req, res) => {
+  console.log("item type: " + req.body.itemType);
+  console.log("item name: " + req.body.itemName);
+  console.log("item price: " + req.body.itemPrice);
+
+  const db = dbConnect.getDbConnectInstance();
+  try {
+    db.updatePrice(req.body.itemType, req.body.itemName, req.body.itemPrice);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// read
+app.post("/viewTable", (req, res) => {
+  const db = dbConnect.getDbConnectInstance();
+  console.log("body: " + req.body.tableName);
+  const result = db.getTableData(String(req.body.tableName));
+
+  result
+    .then((data) => res.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
+app.post("/restockItem", (req, res) => {
+  console.log("item type: " + req.body.itemType);
+  console.log("item name: " + req.body.itemName);
+  console.log("item amount: " + req.body.itemAmount);
+  const db = dbConnect.getDbConnectInstance();
+
+  try {
+    db.restockItem(req.body.itemType, req.body.itemName, req.body.itemAmount);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// update
+
+app.post("/processOrder", (req, res) => {
+  const db = dbConnect.getDbConnectInstance();
+
+  const crust = req.body.crust;
+  const sauce = req.body.sauce;
+  const cheese = req.body.cheese;
+  const drink = req.body.drink; // if none: null
+  const toppings = req.body.toppings; // if none: null
+
+  try {
+    db.processOrder(crust, sauce, cheese, drink, toppings);
+  } catch (error) {
+    console.log(error);
+  }
+});
